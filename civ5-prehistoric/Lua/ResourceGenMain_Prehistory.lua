4a0aaac46a77dbdae28c291bd9125c03473df43c
carlos <github@hernandezweb.com>
2013-12-11 15:22:34 -0800

    final v0.2.5 files, cleanup buildings structure, and preparation for v0.2.6 release

---
+++ civ5-prehistoric/Lua/ResourceGenMain_Prehistory.lua
@@ -0,0 +1,14 @@
+-- Author: FramedArchitecture
+-- DateCreated: 4/21/2013
+--------------------------------------------------------------
+include( "AddResources_Prehistory.lua" );
+
+local g_SaveData = Modding.OpenSaveData();
+function Initialize()
+	local bInit = g_SaveData.GetValue("NEW_ResourcesPlaced");
+	if not bInit then
+		PlaceModResources();
+		g_SaveData.SetValue("NEW_ResourcesPlaced", true);
+	end
+end
+Initialize();
