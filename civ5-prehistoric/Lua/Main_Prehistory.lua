b0a0160f060ce21d92e8ab2c848cf6b97dfeccf8
carlos <github@hernandezweb.com>
2014-01-14 07:16:21 -0800

    Causeway and lua for city plot resource detection

---
+++ civ5-prehistoric/Lua/Main_Prehistory.lua
@@ -0,0 +1,21 @@
+-- Main_Prehistory
+-- Author: carlos
+-- DateCreated: 1/13/2014 3:33:37 PM
+--------------------------------------------------------------
+
+--[[
+
+We are all nothing but leaves in the wind
+
+end]]--
+
+print("--------------------------------------------------");
+print("    __FILE__ Main_Prehistory");
+print("--------------------------------------------------");
+print("");
+
+-- include("Barbarians/CreateAnimals.lua")
+
+include( "Buildings_Prehistory.lua" );
+
+
